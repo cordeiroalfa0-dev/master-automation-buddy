06d2e8ae2ea083bcd113f426d1276a0e7b45e7f8
cordeiroalfa0-dev <cordeiroalfa0@gmail.com>
2026-06-04 17:44:18 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
-    TanStackRouterVite(),
+    tanstackStart({ target: "vercel" }),
     react(),
     tailwindcss(),
     tsconfigPaths(),
